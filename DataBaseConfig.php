7667d538a30736f2e5980aa9c66d1edb7918a185
Likhit Kumar V P <84926428+Likhit-Kumar@users.noreply.github.com>
2023-10-15 18:06:55 +0530

    Add files via upload

---
+++ DataBaseConfig.php
@@ -0,0 +1,21 @@
+<?php
+
+class DataBaseConfig
+{
+    public $servername;
+    public $username;
+    public $password;
+    public $databasename;
+
+    public function __construct()
+    {
+
+        $this->servername = 'abhisekpanda.com';
+        $this->username = 'abhisekp_pratyus';
+        $this->password = 'Pmsl@2021';
+        $this->databasename = 'abhisekp_hospital';
+
+    }
+}
+
+?>
